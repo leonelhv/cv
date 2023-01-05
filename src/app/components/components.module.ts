81f8a51de206622f614d2d8ce629fb5c90508918
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-05 14:40:16 -0500

    import ReactiveFormsModule

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Componentes
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
@@ -20,7 +23,7 @@ import { ContactoComponent } from './contacto/contacto.component';
     ProyectosComponent,
     ContactoComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [
     HeaderComponent,
     MainComponent,
