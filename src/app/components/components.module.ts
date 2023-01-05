f82e693ab83700280871231ed421257b65d269c6
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-05 08:47:13 -0500

    update export and import modules

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,16 +4,9 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    MainComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, MainComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, MainComponent, FooterComponent],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
