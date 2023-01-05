487226b0ce61a54313b916fd686759c84e8c0faa
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-05 09:47:35 -0500

    add tecnologias section

--- src/app/components/tecnologias/tecnologias.component.ts
+++ src/app/components/tecnologias/tecnologias.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface tecnologia {
+  nombre: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-tecnologias',
   templateUrl: './tecnologias.component.html',
-  styleUrls: ['./tecnologias.component.css']
+  styleUrls: ['./tecnologias.component.css'],
 })
 export class TecnologiasComponent {
-
+  tecnologias: tecnologia[] = [
+    {
+      nombre: 'Javascript',
+      img: 'assets/tecnologias/javascript.svg',
+    },
+    {
+      nombre: 'HTML',
+      img: 'assets/tecnologias/html-5.svg',
+    },
+    {
+      nombre: 'CSS',
+      img: 'assets/tecnologias/css-3.svg',
+    },
+    {
+      nombre: 'Angular',
+      img: 'assets/tecnologias/angular-icon.svg',
+    },
+    {
+      nombre: 'Bootstrap',
+      img: 'assets/tecnologias/bootstrap.svg',
+    },
+    {
+      nombre: 'Tailwind',
+      img: 'assets/tecnologias/tailwindcss-icon.svg',
+    },
+    {
+      nombre: 'Typescript',
+      img: 'assets/tecnologias/typescript-icon.svg',
+    },
+  ];
 }
